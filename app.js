413f027fae238fa20d4945e3738b0e217c25546b
memo3000 <32827848+memo3000@users.noreply.github.com>
2022-03-14 17:22:20 +0900

    3월 14일

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add("HIDDEN_CLASSNAME");
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    console.log(username);
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
